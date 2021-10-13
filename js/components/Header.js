3ef3339a7c51d5da92b3c9e85354d1d57fc2d96a
Chloé R <clositi@live.fr>
2021-10-13 16:26:44 +0200

    modified header to had h1

--- js/components/Header.js
+++ js/components/Header.js
@@ -3,7 +3,8 @@ export default class Header {
 	render() {
 		return `
 				<a href="index.html" class="header__link">
-					<img src="img/logo.png" alt="Les petits plats home page" class="link__logo">
+					<img src="img/logo.svg" alt="Les petits plats home page" class="link__logo">
+					<h1 class="link__title">Les petits plats</h1>
 				</a>`;
 	}
 }
